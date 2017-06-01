0cb5936ac939b67277dd7a7468479ffed3a78b55
Bruno <bruno@bettacom.com.br>
2017-06-01 18:55:38 -0300

    Seize image randomly

--- app/src/main/java/caracoroa/cursoandroid/com/caraoucoroa/MainActivity.java
+++ app/src/main/java/caracoroa/cursoandroid/com/caraoucoroa/MainActivity.java
@@ -1,13 +1,36 @@
 package caracoroa.cursoandroid.com.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView jogar;
+    private String[] opcao = {"cara", "coroa"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jogar = (ImageView) findViewById(R.id.btnJogar);
+        jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //gerar numero aleatorio
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(2); //0,1
+
+                Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                intent.putExtra("opcao", opcao[numeroAleatorio]);
+                // envia dados para segunda activity
+                startActivity(intent);
+            }
+        });
+
     }
 }
